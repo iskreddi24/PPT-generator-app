5e8ef8a4aa36eb778d6c376a8a06e510d78d6641
iskreddi24 <sivakrishnareddyinduri@gmail.com>
2025-11-28 13:49:53 +0530

    Fix Vercel deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       // This is the key change: we're telling the React plugin
+//       // to use Emotion's Babel plugin for compilation.
+//       jsxImportSource: '@emotion/react',
+//       babel: {
+//         plugins: ['@emotion/babel-plugin'],
+//       },
+//     }),
+//   ],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // This is the key change: we're telling the React plugin
-      // to use Emotion's Babel plugin for compilation.
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-  ],
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+  }
 })
